7f32e792b84c1af65e93a4dbfd9d2e14f25e0a15
Alessandro David <alessandrodp2222@gmail.com>
2024-06-30 20:14:37 -0500

    episode 2: jsx, components & props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> Hello World
-    </div>
-  );
+      <div className="App">
+        <Job salary={90000} position="Senior SDE" company="Amazon"/>
+        <Job salary={12000} position="Junior SDE" company="Google"/>
+        <Job salary={10000} position="Project Manager" company="Netflix"/>
+      </div>
+);
+}
+
+const Job = (props) => {
+  return <div>
+    <h1>Salary: {props.salary}</h1>
+    <h1>Position: {props.position}</h1>
+    <h1>Company: {props.company}</h1>
+  </div>
+}
+
+
+const User = (props) => {   //props to create reusable components
+  return <div><h1> {props.name} </h1> <h1>{props.age}</h1> <h1>{props.email}</h1></div>
+}
+
+const GetName = ()  => {
+  return "Alessandro"; //this is just a variable
+}
+
+const GetNameComponent = ()  => {
+  return <h1>Alessandro</h1>; //this is a react component
 }
 
 export default App;
