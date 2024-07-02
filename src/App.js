673e27bd9bab3feefd341983d3922cf373d3af84
Alessandro David <alessandrodp2222@gmail.com>
2024-07-02 17:09:39 -0500

    conditional rendering pt 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import styles from './App.module.css';
+import { User } from './User'
 
 function App() {
-    const names  = ["Pedro", "Jake", "Jessica", "Mike", "Dustin"];
+    const users  = [
+        {name: "Pedro", age: 21},
+        {name: "Jake", age: 25},
+        {name: "Jessica", age: 45},
+    ];
 
   return (
       <div className={styles.App}>
-          {names.map((name, key) => {
-              return <h1 key={key}> {name} </h1>
+          {users.map((user, key) => {
+              return <User name={user.name} age={user.age} />;
           })}
       </div>
-);
+  );
 }
 
 export default App;
