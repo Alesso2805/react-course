ab3f5853d76025a84742b1a06f9306ef22e39c76
Alessandro David <alessandrodp2222@gmail.com>
2024-07-09 00:31:17 -0500

    episode 5: CRUD in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import styles from './App.module.css';
-import { User } from './User'
-import { Planets } from './Planets'
-
+import "./App.css"
+import {useState} from "react";
+import { Task } from "./Task";
 function App() {
-    const planets  = [
-        {name: "Mars", isGasPlanet: false},
-        {name: "Earth", isGasPlanet: false},
-        {name: "Jupiter", isGasPlanet: true},
-        {name: "Venus", isGasPlanet: false},
-        {name: "Neptune", isGasPlanet: true},
-        {name: "Uranus", isGasPlanet: true},
-    ];
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) =>  task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+        todoList.map((task) => {
+          if (task.id === id) {
+            return { ...task, completed: true };
+          } else {
+            return task;
+          }
+        })
+    );
+  };
 
   return (
-      <div className={styles.App}>
-          {planets.map((planet, key) => {
-              if (planet.isGasPlanet === true){
-                  return <Planets name={planet.name}/>;
-              }
-          })}
+      <div className="App">
+        <div className="addTask">
+          <input type="text" onChange={handleChange}/>
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        <div className="list">
+          {todoList.map((task) => {
+            return <Task taskName ={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask}></Task>
+        })}
+        </div>
       </div>
   );
 }
