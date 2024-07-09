5d4004bca496f4cc3a59e88dcfaf3a17990089d0
Alessandro Alonso David Polanco <u202122232@upc.edu.pe>
2024-07-09 17:44:36 -0500

    episode 7: fetching data from API to get predicting ages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
-import axios from 'axios';
-import {useEffect, useState} from "react";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
-    const [catFact, setCatFact] = useState("");
+    const [name, setName] = useState("");
+    const [predictedAge, setPredictedAge] = useState(null);
 
-    const fetchCatFact = () => {
-        axios.get("https://catfact.ninja/fact").then((res) => {setCatFact(res.data.fact)
+    const fetchData = () => {
+        Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+            console.log(res.data);
         });
-    }
-
-    useEffect(() => {
-        fetchCatFact()
-    }, []);
+    };
 
   return (
-    <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p>{catFact}</p>
-    </div>
+      <div className="App">
+          <input
+              placeholder="Ex. Pedro..."
+              onChange={(event) => {
+                  setName(event.target.value);
+              }}
+          />
+          <button onClick={fetchData}>Predict Age</button>
+
+          <h1> Name: {predictedAge?.name}</h1>
+          <h1> Predicted age: {predictedAge?.age}</h1>
+          <h1> Count: {predictedAge?.count}</h1>
+      </div>
   );
 }
 
