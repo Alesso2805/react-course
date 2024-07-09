03a0dd617ac9ef15f8780fb0bc48bf5e369a1ce6
Alessandro Alonso David Polanco <u202122232@upc.edu.pe>
2024-07-09 15:27:36 -0500

    example 1: cat fact generator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [catFact, setCatFact] = useState("");
+
+    const fetchCatFact = () => {
+        axios.get("https://catfact.ninja/fact").then((res) => {setCatFact(res.data.fact)
+        });
+    }
+
+    useEffect(() => {
+        fetchCatFact()
+    }, []);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
+    </div>
   );
 }
 
