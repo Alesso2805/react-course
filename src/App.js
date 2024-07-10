68cd628bd5807c89bf3bfacd1d4be10459434617
Alessandro David <alessandrodp2222@gmail.com>
2024-07-10 00:05:25 -0500

    episode 8: creating routes, error page, navbar component with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {Menu} from "./pages/Menu";
+import {Contact} from "./pages/Contact";
+import {NavBar} from "./pages/NavBar";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/menu" element={<Menu></Menu>} />
+          <Route path="/contact" element={<Contact></Contact>} />
+          <Route path="*" element={<h1> ERROR 404 - PAGE NOT FOUND </h1>} />
+        </Routes>
+    </Router></div>
   );
 }
 
