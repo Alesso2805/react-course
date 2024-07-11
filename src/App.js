b87743f4f928222cde373aead41b4dec05eb1567
Alessandro David <alessandrodp2222@gmail.com>
2024-07-11 12:18:35 -0500

    custom hook example refetching cat data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Cat } from "./Cat";
 import './App.css';
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App"></div>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <Cat />
+        </div>
+      </QueryClientProvider>
   );
 }
 
